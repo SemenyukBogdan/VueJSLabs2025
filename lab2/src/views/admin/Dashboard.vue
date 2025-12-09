6f9485e7aa224a29deb68f483a936b11724a4deb
Bogdan <vt222_sbp@student.ztu.edu.ua>
2025-12-09 14:57:42 +0200

    feat(dashboard): lab2 3.5 add KeepAlive tabs with include/exclude/max demo

--- lab2/src/views/admin/Dashboard.vue
+++ lab2/src/views/admin/Dashboard.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
+import { shallowRef } from 'vue'
+import CompA from '@/components/CompA.vue'
+import CompB from '@/components/CompB.vue'
+
+const current = shallowRef(CompA)
+
+
 
 </script>
 
 <template>
-
+  <div class="demo">
+    <label><input type="radio" v-model="current" :value="CompA" /> A</label>
+    <label><input type="radio" v-model="current" :value="CompB" /> B</label>
+    <KeepAlive include="CompA" exclude="CompB" :max="2">
+      <component :is="current"></component>
+    </KeepAlive>
+  </div>
 </template>
+
 
 <style scoped>
 
